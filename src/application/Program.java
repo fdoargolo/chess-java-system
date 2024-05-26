0c0bafbbd59eb8164f99277e47510f7000836be1
fdoargolo <felipeargolo75@gmail.com>
2024-05-26 14:33:10 -0300

    First class: Position, and gitinore

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Position position = new Position(2,1);
+		System.out.println(position);
 	}
 
 }
